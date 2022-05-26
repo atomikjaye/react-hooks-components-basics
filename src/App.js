7db8c74c6206fc969c58fe70780db5328795f6a4
Jocelyn Naarden <tzionone@gmail.com>
2022-05-25 20:43:55 -0400

    I don't think there was a change that needed to be made in the lesson but I tested something through the React Docs!

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+const Button = props => {
+  console.log(props)
+  // Testing somrthing from React Docs https://reactjs.org/docs/jsx-in-depth.html#user-defined-components-must-be-capitalized
+  const { kind, ...other } = props;
+  const className = kind === "second" ? "PrimaryButton" : "SecondaryButton";
+  return <button className={className} {...other} />;
+};
+
+
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Button kind="primary" keys="keyss" onClick={() => console.log("clicked!")}>
+        Hello World!
+      </Button>
     </div>
   );
 }
